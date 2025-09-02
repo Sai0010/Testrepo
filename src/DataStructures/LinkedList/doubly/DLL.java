d52a42fbc27f82a14aefc4d7086063aadd98429e
Sai0010 <chirag07115@gmail.com>
2025-09-02 12:48:28 +0530

    added public class DLL

---
+++ src/DataStructures/LinkedList/doubly/DLL.java
@@ -0,0 +1,19 @@
+package DataStructures.LinkedList.doubly;
+class Node{
+    int data;
+    Node next;
+    Node prev;
+
+    Node(int data){
+        this.data=data;
+        this.next=null;
+        this.prev= null;
+    }
+}
+public class DLL {
+    Node head;
+
+    DLL() {
+        head = null;
+    }
+}
